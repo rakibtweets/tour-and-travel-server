3f8fd38551d089d6ed022dd6e84b90508ce226f0
rakibtweets <rakibhasanroky0@gmail.com>
2021-10-31 15:41:30 +0600

    meaningful comments

--- index.js
+++ index.js
@@ -11,14 +11,15 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+// mongodb uri connection
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.vncsz.mongodb.net/myFirstDatabase?retryWrites=true&w=majority`;
-// console.log('~ uri', uri);
 
 const client = new MongoClient(uri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
 });
 
+// CRUD operation starts from here
 async function run() {
   try {
     await client.connect();
